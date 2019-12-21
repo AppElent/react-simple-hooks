bca5e9e7e03c1a33775b679aed4e86377849b2db
Eric Jansen <ericjansen@live.nl>
2019-12-21 11:41:02 +0100

    initial version

---
+++ src/index.tsx
@@ -0,0 +1,12 @@
+import * as React from "react";
+
+export interface HelloProps { compiler: string; framework: string; }
+
+export const Hello = (props: HelloProps) => <h1>Hello from {props.compiler} and {props.framework}!</h1>;
+
+export { default as useForm } from './useForm';
+export { default as useLocalStorage } from './useLocalStorage';
+export { default as useMaterialUIMenu } from './useMaterialUIMenu';
+export { default as useModal } from './useModal';
+export { default as useStateExtended } from './useStateExtended';
+export { default as useTabs } from './useTabs';
--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,11 @@ var path = require('path');
 
 module.exports = {
     mode: 'production',
-    entry: './src/index.js',
+    entry: './src/index.tsx',
+    resolve: {
+        // Add '.ts' and '.tsx' as resolvable extensions.
+        extensions: [".ts", ".tsx", ".js", ".jsx"]
+    },
     output: {
         path: path.resolve('lib'),
         filename: 'index.js',
@@ -11,10 +15,24 @@ module.exports = {
     module: {
         rules: [
             {
-                test: /\.jsx?$/,
+                test: /\.ts(x?)$/,
+                exclude: /(node_modules)/,
+                use: 'ts-loader'
+            },
+            {
+                test: /\.js(x?)$/,
                 exclude: /(node_modules)/,
                 use: 'babel-loader'
             }
         ]
+    },
+
+    // When importing a module whose path matches one of the following, just
+    // assume a corresponding global variable exists and use that instead.
+    // This is important because it allows us to avoid bundling all of our
+    // dependencies, which allows browsers to cache those libraries between builds.
+    externals: {
+        "react": "React",
+        "react-dom": "ReactDOM"
     }
 }
